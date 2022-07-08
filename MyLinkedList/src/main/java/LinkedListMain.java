307fb61f85d83e551bb316e653528ceda01c57f2
Suhas Padikar <108081981+SuhasPadikar@users.noreply.github.com>
2022-07-08 19:08:38 +0530

    Add files via upload

---
+++ MyLinkedList/src/main/java/LinkedListMain.java
@@ -0,0 +1,12 @@
+public class LinkedListMain
+{
+ public static void main(String[] args)
+ {
+   Linkedlist list = new Linkedlist();
+   list.insert(18);
+   list.insert(45);
+   list.insert(12);
+   list.show();
+
+ }
+}
---
+++ MyLinkedList/src/test/java/LinkedlistTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedlistTest {
+
+    @org.junit.jupiter.api.Test
+    void insert()
+    {
+        System.out.println("Testing method insert");
+    }
+
+    @org.junit.jupiter.api.Test
+    void show()
+    {
+        System.out.println("Testing method show");
+    }
+}
